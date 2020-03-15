21f65fa0d44a30a914dcb8eb2e5472b6dbbfe443
Aryan Soni <mail.aryansoni@gmail.com>
2020-03-15 11:36:53 -0400

    config components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar";
+import Resume from "./components/resume";
+import Skills from "./components/skills";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+import resumeInfo from "./resumeInfo";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      // render components; pass resumeInfo into each respective component
+      <Navbar resumeInfo={resumeInfo} />
+      <Resume resumeInfo={resumeInfo} />
+      <Skills resumeInfo={resumeInfo} />
+      <Contact resumeInfo={resumeInfo} />
+      <Footer resumeInfo={resumeInfo} />
     </div>
   );
 }
