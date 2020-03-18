421edc3f55a74b8f6a2bc14e7c613f3f81490846
Aryan Soni <mail.aryansoni@gmail.com>
2020-03-18 17:30:17 -0400

    final changes

--- src/components/about.js
+++ src/components/about.js
@@ -18,17 +18,19 @@ function About() {
           <div class='row'>
             <div class='col-sm p-3'>
               <h4 class='font-weight-bold'>About Me</h4>
-              Hi, my name is Aryan — welcome to my personal website! I am a high
-              school student at Heart Lake Secondary School who has cultivated a
-              strong interest in computer science and entrepreneurship. I like
-              using software as a tool to solve problems, and am interested in
-              the applications of software across various fields, including
-              finance and e-commerce.
-              <br />
-              <br />
-              Enclosed is a link to my resume. You'll also see a link to my
-              GitHub, where you can check out some of the projects I have worked
-              on.
+              <p id='aboutText'>
+                Hi, my name is Aryan — welcome to my personal website! I am a
+                high school student at Heart Lake Secondary School who has
+                cultivated a strong interest in computer science and
+                entrepreneurship. I like using software as a tool to solve
+                problems, and am interested in the applications of software
+                across various fields, including finance and e-commerce.
+                <br />
+                <br />
+                Enclosed is a link to my resume. You'll also see a link to my
+                GitHub, where you can check out some of the projects I have
+                worked on.
+              </p>
             </div>
             <div class='col-sm pt-4 aboutLink'>
               <button
